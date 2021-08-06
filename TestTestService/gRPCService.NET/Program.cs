116963bc673dc11cd8982cb027e2ab7a8a83c2b1
ClaudioTheo <claudio.theobaldo@seventh.com.br>
2021-08-06 10:15:05 -0300

    [gRPC-01] Adding port parameter to the C# server

--- TestTestService/gRPCService.NET/Program.cs
+++ TestTestService/gRPCService.NET/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace gRPCService.NET
 {
@@ -21,7 +22,16 @@ namespace gRPCService.NET
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    UInt16 vPort = 5000;
+                    if (args.Length >= 1)
+                        vPort = UInt16.Parse(args[0]);
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls($"http://localhost:{vPort}", $"https://localhost:{vPort+1}");
+                    //webBuilder.UseKestrel(opts =>
+                    //{
+                    //    opts.Listen(IPAddress.Loopback, port: vPort);
+                    //});
                 });
     }
 }
